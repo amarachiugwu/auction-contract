1bd235b130d337a6bea9f19971f77d4a60c03584
amarachiugwu <marycynthiamara2@gmail.com>
2022-08-25 00:19:04 +0100

    auction contract

---
+++ contracts/Auction.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.9;
+
+interface IERC721 {
+    function transferFrom (
+        address from,
+        address to,
+        uint tokenId
+    ) external;
+} 
+
+contract Auction {
+    // ERC721 Interface
+    // start auction by the seller
+    // bid function
+    // store failded bids
+    // end auction by anybody
+    // withdraw failed bids
+
+
+
+    address payable public seller;
+    address public nftAddr;
+    uint public nftId;
+    uint public minBid;
+    uint public highestBid;
+    address public highestBidder;
+    bool public started;
+    bool public ended;
+    uint32 deadline = uint32(block.timestamp + 90);
+
+    mapping(address => uint) bids;
+
+    event Start(address starter);
+    event Bid (address bidder, uint amt);
+    event End (address ender, address highestBidder, uint highestBid);
+    event Withdraw(address addr, uint amt);
+
+    constructor (
+        uint _nftId,
+        uint _minBid
+
+    ) {
+        seller = payable(msg.sender);
+        nftId = _nftId;
+        minBid = _minBid;
+    }
+
+    function startAuction () external {
+        require (!started, "auction started");
+        require (msg.sender == seller, "not seller");
+
+        started = true;
+
+        IERC721 nft = IERC721(nftAddr);
+        nft.transferFrom(seller, address(this), nftId);
+
+        emit Start (msg.sender);
+    }
+
+    function bid () payable external{
+        require(started, "not started");
+        require(block.timestamp < deadline, "Auction ended");
+        require(msg.value >= minBid && msg.value > highestBid, "Bid not accepted");
+
+        if(highestBidder != address(0)){
+            bids[highestBidder] += highestBid;
+        }
+
+        highestBid = msg.value;
+        highestBidder = msg.sender;
+
+        emit Bid (msg.sender, msg.value);
+    }
+
+    function endAuction () external{
+        require(!started, "not started");
+        require(block.timestamp >= deadline, "time not reached");
+        require(!ended, "Auction Ended");
+
+        ended = true;
+
+        IERC721 nft = IERC721(nftAddr);
+        nft.transferFrom(address(this), highestBidder, nftId);
+        seller.transfer(highestBid);
+
+        emit End (msg.sender, highestBidder, highestBid);
+    }
+
+    function withdraw () external {
+        uint balance = bids[msg.sender];
+        require(balance > 0, "You have no bid");
+        balance = 0;
+        payable(msg.sender).transfer(balance);
+
+        emit Withdraw (msg.sender, balance);
+    }
+}
